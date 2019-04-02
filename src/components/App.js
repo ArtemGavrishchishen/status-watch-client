3ad5ef52eb43ef7c5fe88231d1e6a540d8d9ea1f
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2019-04-02 21:20:25 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="App">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </div>
+    );
+  }
+}
+
+export default App;
