f98f8ebe743080142d17d4b8d333bef34b0028e9
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2019-04-18 15:12:06 +0300

    refact topBar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import queryString from 'query-string';
+
+import Selected from '../Selected/Selected';
+import Pagination from '../Pagination/Pagination';
+
+const optionsSort = [
+  { value: 'hit', label: 'sorted by hit' },
+  { value: 'new', label: 'sorted by new' },
+  { value: 'cheap', label: 'from cheap to expensive' },
+  { value: 'expensive', label: 'from expensive to cheap' },
+];
+
+const optionsView = [
+  { value: '12', label: 'View by 12' },
+  { value: '15', label: 'View by 15' },
+];
+
+const getSelectedOption = (select, options) =>
+  options.find(option => option.value === select);
+
+class TopBar extends Component {
+  state = {};
+
+  onSortChange = selected => {
+    const { history, location } = this.props;
+    const parsed = queryString.parse(location.search);
+    let stringified;
+
+    if (selected !== null) {
+      parsed.sort = selected.value;
+      stringified = queryString.stringify(parsed);
+    }
+
+    if (selected === null) {
+      const { sort, ...newParsed } = parsed;
+      stringified = queryString.stringify(newParsed);
+    }
+
+    history.push({
+      pathname: location.pathname,
+      search: stringified,
+    });
+  };
+
+  onViewChange = selected => {
+    const { history, location } = this.props;
+    const parsed = queryString.parse(location.search);
+    let stringified;
+
+    if (selected !== null) {
+      parsed.view = selected.value;
+      stringified = queryString.stringify(parsed);
+    }
+
+    if (selected === null) {
+      const { view, ...newParsed } = parsed;
+      stringified = queryString.stringify(newParsed);
+    }
+
+    history.push({
+      pathname: location.pathname,
+      search: stringified,
+    });
+  };
+
+  render() {
+    const { location } = this.props;
+    const parsed = queryString.parse(location.search);
+    return (
+      <>
+        <Selected
+          selectedOption={getSelectedOption(parsed.sort, optionsSort)}
+          handleChange={this.onSortChange}
+          options={optionsSort}
+          placeholder="Sorted by ..."
+        />
+        <Selected
+          selectedOption={getSelectedOption(parsed.view, optionsView)}
+          handleChange={this.onViewChange}
+          options={optionsView}
+          placeholder="View by ..."
+        />
+        <Pagination />
+      </>
+    );
+  }
+}
+
+export default withRouter(TopBar);
--- src/pages/WatchesPage.js
+++ src/pages/WatchesPage.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Selected from '../components/Selected/Selected';
-import Pagination from '../components/Pagination/Pagination';
+import TopBar from '../components/TopBar/TopBar';
 import WatchesGrid from '../components/WatchesGrid/WatchesGrid';
 
 import { watchesSelectors, watchesOperations } from '../redux/watches';
 
 import styles from './WatchesPage.module.css';
 
-const optionsSort = [
-  { value: 'Sorted by 1', label: 'Sorted by 1' },
-  { value: 'Sorted by 2', label: 'Sorted by 2' },
-  { value: 'Sorted by 3', label: 'Sorted by 3' },
-];
-
-const optionsView = [
-  { value: 'View by 1', label: 'View by 1' },
-  { value: 'View by 2', label: 'View by 2' },
-  { value: 'View by 3', label: 'View by 3' },
-];
-
 class WatchesPage extends Component {
-  state = {
-    selectedSort: null,
-    selectedView: null,
-  };
+  state = {};
 
   componentDidMount() {
     const { fetchWatches } = this.props;
     fetchWatches();
   }
 
-  handleChangeSort = selected => this.setState({ selectedSort: selected });
-
-  handleChangeView = selected => this.setState({ selectedView: selected });
-
   render() {
-    const { selectedSort, selectedView } = this.state;
     const { watches } = this.props;
     return (
       <main>
@@ -46,19 +25,7 @@ class WatchesPage extends Component {
             <div className={styles.leftBar}>1</div>
             <div className={styles.content}>
               <div className={styles.topBar}>
-                <Selected
-                  selectedOption={selectedSort}
-                  handleChange={this.handleChangeSort}
-                  options={optionsSort}
-                  placeholder="Sorted by ..."
-                />
-                <Selected
-                  selectedOption={selectedView}
-                  handleChange={this.handleChangeView}
-                  options={optionsView}
-                  placeholder="View by ..."
-                />
-                <Pagination />
+                <TopBar />
               </div>
               <WatchesGrid items={watches} />
             </div>
